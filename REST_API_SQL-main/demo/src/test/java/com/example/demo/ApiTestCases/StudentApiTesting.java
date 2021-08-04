fabef2ad4612c7b40df8a2496ad29b54d7551b70
Ahmad-Hamad1 <ahmad_hamad1@outlook.com>
2021-08-04 14:52:07 +0300

    adding comment

--- REST_API_SQL-main/demo/src/test/java/com/example/demo/ApiTestCases/StudentApiTesting.java
+++ REST_API_SQL-main/demo/src/test/java/com/example/demo/ApiTestCases/StudentApiTesting.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// test
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentApiTesting {
